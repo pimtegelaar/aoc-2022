239bd2e5778a1561959db5d7ec5663ee27079962
pimtegelaar <pimtegelaar@gmail.com>
2022-12-06 09:29:09 +0100

    Day 6

---
+++ src/Day06.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+    fun String.isUnique() = length == toSet().size
+
+    fun uniqueAfter(input: String, size: Int): Int {
+        val first = input.windowed(size).first { it.isUnique() }
+        return input.indexOf(first) + size
+    }
+
+    fun part1(input: String) = uniqueAfter(input,4)
+
+    fun part2(input: String) = uniqueAfter(input,14)
+
+    val testInput = readInputAsString("Day06_test")
+    val input = readInputAsString("Day06")
+
+    val part1 = part1(testInput)
+    check(part1 == 7) { part1 }
+    println(part1(input))
+
+    val part2 = part2(testInput)
+    check(part2 == 19) { part2 }
+    println(part2(input))
+}
